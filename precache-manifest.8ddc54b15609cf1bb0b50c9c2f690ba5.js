123adfb902bc59e4d3baee7c65540cc1f749f8a8
Tobi <tobiatkoko@Tobis-MacBook-Pro.local>
2024-07-25 12:36:53 +0100

    Updates

---
+++ precache-manifest.8ddc54b15609cf1bb0b50c9c2f690ba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3d8582c64428f2b01953e60fcf9dc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "194ba5fce4b249982f63",
+    "url": "/static/css/main.d557d980.chunk.css"
+  },
+  {
+    "revision": "69d7dd0521ffe2367ac1",
+    "url": "/static/js/2.7bdfe267.chunk.js"
+  },
+  {
+    "revision": "194ba5fce4b249982f63",
+    "url": "/static/js/main.59d256fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2403fa3304936d9e834a986c46262fc0",
+    "url": "/static/media/home-v.2403fa33.png"
+  }
+]);
